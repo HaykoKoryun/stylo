d3efad7d3d00f2c88c660a9496b84ea6461318b6
Hayko Koryun <haykokoryun@gmail.com>
2016-01-11 07:00:46 +0400

    change tabs to 2 spaces

--- styler.js
+++ styler.js
@@ -1,46 +1,46 @@
 (function()
 {
-    var Styler = function()
+  var Styler = function()
+  {
+    var _instance = this;
+
+    var _styleSheet = document.createElement("style");
+
+    document.head.appendChild(_styleSheet);
+
+    var _rules = {};
+
+    _instance.update = function()
     {
-        var _instance = this;
+      var stylesheet = "";
 
-        var _styleSheet = document.createElement("style");
-		
-        document.head.appendChild(_styleSheet);
+      for (var selector in _rules)
+      {
+        stylesheet += selector + "{";
+        var rules = _rules[selector];
 
-        var _rules = {};
-
-        _instance.update = function()
+        for (var prop in rules)
         {
-            var stylesheet = "";
-
-            for(var selector in _rules)
-            {
-                stylesheet += selector + "{";
-                var rules = _rules[selector];
-
-                for(var prop in rules)
-                {
-                    stylesheet += prop + ":" + rules[prop] + ";";
-                }
-
-                stylesheet += "}";
-            }
-
-            _styleSheet.textContent = stylesheet;
+          stylesheet += prop + ":" + rules[prop] + ";";
         }
 
-        _instance.addRule = function(selector, rules)
-        {
-            if(_rules[selector] == undefined)
-            {
-                _rules[selector] = {};
-            }
-            for(var rule in rules)
-            {
-                _rules[selector][rule] = rules[rule];
-            }
-        }
+        stylesheet += "}";
+      }
+
+      _styleSheet.textContent = stylesheet;
     }
-    this.Styler = Styler;
+
+    _instance.addRule = function(selector, rules)
+    {
+      if (_rules[selector] == undefined)
+      {
+        _rules[selector] = {};
+      }
+      for (var rule in rules)
+      {
+        _rules[selector][rule] = rules[rule];
+      }
+    }
+  }
+  this.Styler = Styler;
 })();
